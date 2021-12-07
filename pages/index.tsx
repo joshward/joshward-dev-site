ac16068b2dabca9f1f1fd0d71357b44b5666e567
Josh Ward <joshwardcodes@gmail.com>
2021-12-06 21:37:18 -0800

    add missed save

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,24 +13,6 @@ const Home: NextPage = () => {
     >
       <Head>
         <title>Josh Ward</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        ></link>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        ></link>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        ></link>
-        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <Header />
     </div>
